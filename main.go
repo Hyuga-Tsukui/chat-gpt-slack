bcd16812dd275bd36fd4728b0c9c6fa2935e09f2
Hyuga-Tsukui <gakikiki407@gmail.com>
2023-03-24 08:28:22 +0900

    slack

--- main.go
+++ main.go
@@ -1,48 +1,49 @@
 package main
 
 import (
-	// "encoding/json"
+	"encoding/json"
 	"fmt"
-	// "io/ioutil"
+	"io/ioutil"
 	"net/http"
-	// "os"
-	// "github.com/slack-go/slack"
-	// "github.com/slack-go/slack/slackevents"
+	"os"
+
+	"github.com/slack-go/slack"
+	"github.com/slack-go/slack/slackevents"
 )
 
 func handle(w http.ResponseWriter, r *http.Request) {
 
-	// signingSecret := os.Getenv("SLACK_SIGNING_SECRET")
+	signingSecret := os.Getenv("SLACK_SIGNING_SECRET")
 
-	// body, err := ioutil.ReadAll(r.Body)
-	// if err != nil {
-	// 	w.WriteHeader(http.StatusBadRequest)
-	// 	return
-	// }
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 
-	// //  Salckからのリクエストであるかどうかを検証する
-	// sv, err := slack.NewSecretsVerifier(r.Header, signingSecret)
-	// if err != nil {
-	// 	w.WriteHeader(http.StatusInternalServerError)
-	// 	return
-	// }
-	// if _, err := sv.Write(body); err != nil {
-	// 	w.WriteHeader(http.StatusInternalServerError)
-	// 	return
-	// }
-	// if err := sv.Ensure(); err != nil {
-	// 	w.WriteHeader(http.StatusUnauthorized)
-	// 	return
-	// }
+	//  Salckからのリクエストであるかどうかを検証する
+	sv, err := slack.NewSecretsVerifier(r.Header, signingSecret)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	if _, err := sv.Write(body); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	if err := sv.Ensure(); err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
 
-	// // リクエストを検証したら、イベントをパースする
-	// eventsAPIEvent, err := slackevents.ParseEvent(json.RawMessage(body), slackevents.OptionNoVerifyToken())
-	// if err != nil {
-	// 	w.WriteHeader(http.StatusInternalServerError)
-	// 	return
-	// }
+	// リクエストを検証したら、イベントをパースする
+	eventsAPIEvent, err := slackevents.ParseEvent(json.RawMessage(body), slackevents.OptionNoVerifyToken())
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 
-	// println(eventsAPIEvent.Type)
+	println(eventsAPIEvent.Type)
 
 	w.Write([]byte("Hello World"))
 }
